69e88852ba1b9ad16faceea451b7a63fbc5b2822
krigga <krigga7@gmail.com>
2024-01-27 10:55:33 +0300

    feat: better get method return object

--- src/blockchain/BlockchainContractProvider.ts
+++ src/blockchain/BlockchainContractProvider.ts
@@ -63,11 +63,14 @@ export class BlockchainContractProvider implements SandboxContractProvider {
     }
     async get(name: string, args: TupleItem[]): Promise<ContractGetMethodResult> {
         const result = await this.blockchain.runGetMethod(this.address, name, args)
-        return {
+        const ret = {
+            ...result,
             stack: result.stackReader,
-            gasUsed: result.gasUsed,
+            stackItems: result.stack,
             logs: result.vmLogs,
         }
+        delete (ret as any).stackReader
+        return ret
     }
     async external(message: Cell) {
         const init = ((await this.getState()).state.type !== 'active' && this.init) ? this.init : undefined
