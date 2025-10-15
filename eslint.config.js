a2bda9da8b6f991a0425b8777eedb889cdcc161f
krigga <krigga7@gmail.com>
2025-10-15 14:22:28 +0300

    chore: eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,6 @@
 const base = require('@ton/toolchain');
 
-module.exports = [...base, { ignores: ['src/executor/emulator-emscripten*'] }];
+module.exports = [
+    ...base,
+    { ignores: ['src/executor/emulator-emscripten*', 'src/config/defaultConfig.ts', 'src/config/slimConfig.ts'] },
+];
