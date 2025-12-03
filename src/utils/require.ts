47d8e1e21da2b970185926b293a9bafa69b27bc6
Aliaksandr Bahdanau <90352952+Alejandbel@users.noreply.github.com>
2025-12-03 23:18:06 +0300

    feature: remove throw in require (#143)

--- src/utils/require.ts
+++ src/utils/require.ts
@@ -3,11 +3,7 @@ export function requireOptional(id: string): any | undefined {
     try {
         // eslint-disable-next-line @typescript-eslint/no-require-imports
         return require(id);
-    } catch (error) {
-        if (String(error).includes('Cannot find module')) {
-            return undefined;
-        }
-
-        throw error;
+    } catch {
+        return undefined;
     }
 }
